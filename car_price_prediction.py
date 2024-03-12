ba3297d574c37e238515b60c587419b7653121c6
Sainadh-Bahadursha <107560972+Sainadh-Bahadursha@users.noreply.github.com>
2024-03-12 23:55:05 +0530

    update car_price_prediction.py

--- car_price_prediction.py
+++ car_price_prediction.py
@@ -1,3 +1,14 @@
+# Importing the libraries
 import streamlit as st
+import pandas as pd
 
+
+# Heading
 st.header("Car Price Prediction App")
+
+# Using the pandas to import the dataset
+df = pd.read_csv("car_price.csv")
+
+# st.dataframe can be used to print the dataset
+#st.dataframe(df)
+
