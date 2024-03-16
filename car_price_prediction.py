1cb6beef80b0111105c413a6844c11bfea81aa1d
Sainadh-Bahadursha <107560972+Sainadh-Bahadursha@users.noreply.github.com>
2024-03-16 17:07:35 +0530

    Update car_price_prediction.py

--- car_price_prediction.py
+++ car_price_prediction.py
@@ -1,6 +1,7 @@
 # Importing the libraries
 import streamlit as st
 import pandas as pd
+import pickle
 
 
 # Heading
@@ -12,4 +13,17 @@ df = pd.read_csv("car_price.csv")
 # st.dataframe can be used to print the dataset
 # st.dataframe(df)
 
+encode_dict = {
+    "fuel_type" :{"Diesel":1,"Petrol" : 2,"CNG" : 3, "LPG" : 4, "Electric" : 5},
+    "seller_type" : {"Dealer":1, "Individual" : 2, "Trustmark Dealer":3},
+    "transmission_type" : {"Manual" : 1, "Automatic" : 2}
+}
+
+# Loading the final trained ML model which was saved in the format of car_pred.pkl --> Pickle file
+def model_pred():
+    with open("car_pred","rb") as file:
+        model = pickle.load(file)
+
+        input_features = [2014,"seller_type",130000,"Petrol","Manual",19.7,796,46.3,5]
+        return model.predict()
 
